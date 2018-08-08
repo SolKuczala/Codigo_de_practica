19f471327b02b6a813bcd4bff7c7dbddfdbb8cac
SolKuczala <sol.kuczala@gmail.com>
2018-08-08 19:03:06 -0300

    continuacion de ejercicios

--- exerbookchp6.js
+++ exerbookchp6.js
@@ -3,15 +3,15 @@
 //if else condition
 function ifElse(params) {
  
-let name = 'sol';
+    let name = 'sol';
 
-if (name === 'sol') {
-    console.log('hello me');
-} else if (name === Alan){
-    console.log('hi mimi');
-} else {
-    console.log('hi stranger');
-}
+    if (name === 'sol') {
+        console.log('hello me');
+    } else if (name === Alan){
+        console.log('hi mimi');
+    } else {
+        console.log('hi stranger');
+    }
 
 }
 
@@ -26,6 +26,38 @@ function sheepCounter(params) {
         console.log("zzzzzzzzzz");   
     }
 
+
+//printing powers
+
+function forPower(params) {
+    for (let x = 3; x < 10000; x = x * 3) {
+        console.log(x);
+        
+    }
+}
+
+//probar
+function whilePower(params) {
+    let numeroBase = 3;
+    while (numeroBase < 10000) {
+        numeroBase * 3;
+    }
+}
+
+//final excercise chap 6
+
+//awesome animals
+function Aanimals(params) {
+    
+    let animals = ['cat', 'fish', 'lemur', 'komodo dragon'];
+
+    for (let index = 0; index < animals.length; index++) {
+        animals[index]= 'awesome ' + animals[index];
+    }
+    console.log(animals);
+}
+
+//random string generator
 
 
 
